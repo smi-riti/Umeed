c5fde08a311b4bcb7f8c98878b039ce36c6fa1ac
smi-riti <smriti.kumari5104@gmail.com>
2025-09-16 14:21:27 +0530

    livewire and models set-up

---
+++ database/migrations/2025_09_16_084119_create_doctors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('department_id')->nullable()->constrained('departments')->onDelete('set null');
+            $table->index('department_id');
+            $table->string('slug')->nullable();
+            $table->index('slug');
+            $table->bigInteger('fee')->default(500);
+            $table->boolean('status')->default(0);
+            $table->string('image')->nullable();
+            $table->string('image_file_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
